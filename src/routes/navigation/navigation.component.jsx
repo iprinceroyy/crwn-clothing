1640a494299cdedbc2e6dc4a26a7da1c9fafc9c8
Prince Roy <princeku.ray143@gmail.com>
2023-02-22 21:31:08 +0530

    implement reselect library to memoization

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,6 +8,7 @@ import { selectCurrentUser } from '../../store/user/user.selector';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/card-dropdown/cart-dropdown.component';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles.jsx';
 
@@ -25,7 +26,9 @@ const Navigation = () => {
 					<NavLink to='/shop'>SHOP</NavLink>
 
 					{currentUser ? (
-						<NavLink to='/auth'>SIGN OUT</NavLink>
+						<NavLink as='span' className='nav-link' onClick={signOutUser}>
+							SIGN OUT
+						</NavLink>
 					) : (
 						<NavLink to='/auth'>SIGN IN</NavLink>
 					)}
