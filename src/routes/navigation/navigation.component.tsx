c3c90fb072373e7fc2c35403254b3926f6127682
Prince Roy <princeku.ray143@gmail.com>
2023-03-06 01:07:09 +0530

    typed SVG imports

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -9,7 +9,7 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/card-dropdown/cart-dropdown.component';
 
-import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles.jsx';
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles';
 import { signOutStart } from '../../store/user/user.action';
 
 const Navigation = () => {
